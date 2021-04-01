81d567b90f97eb6646bcad76525f2cf0a01edead
ksmithut <github@ksmithut.com>
2021-04-01 06:03:34 -0600

    updates types

--- dist/jwt.d.ts
+++ dist/jwt.d.ts
@@ -1,43 +1,77 @@
 /**
+ * @typedef {object} SignOptions
+ * @property {Date} [now] Date to use for all clock calculations
+ * @property {string} [issuer] identifies the principal that issued the JWT
+ * @property {string} [subject] identifies the principal that is the subject of the JWT
+ * @property {string|string[]} [audience] identifies the recipients that the JWT is intended for
+ * @property {Date|number} [expiresAt]
+ * @property {number} [expiresIn]
+ * @property {Date|number} [notBefore] identifies the time before which the JWT MUST NOT be accepted for processing
+ * @property {boolean|Date|number} [issuedAt=true] identifies the time at which the JWT was issued
+ * @property {string} [jwtId] provides a unique identifier for the JWT
+ */
+/**
  * @param {{[key: string]: any}} payload
  * @param {import('./key-store').KeyStore} keyStore
- * @param {object} [options]
- * @param {Date} [options.now] Date to use for all clock calculations
- * @param {string} [options.issuer] identifies the principal that issued the JWT
- * @param {string} [options.subject] identifies the principal that is the subject of the JWT
- * @param {string|string[]} [options.audience] identifies the recipients that the JWT is intended for
- * @param {Date|number} [options.expiresAt]
- * @param {number} [options.expiresIn]
- * @param {Date|number} [options.notBefore] identifies the time before which the JWT MUST NOT be accepted for processing
- * @param {boolean|Date|number} [options.issuedAt=true] identifies the time at which the JWT was issued
- * @param {string} [options.jwtId] provides a unique identifier for the JWT
+ * @param {SignOptions} [options]
  */
 export function sign(payload: {
     [key: string]: any;
-}, keyStore: import('./key-store').KeyStore, { now, issuer, subject, audience, expiresAt, expiresIn, notBefore, issuedAt, jwtId }?: {
-    now?: Date | undefined;
-    issuer?: string | undefined;
-    subject?: string | undefined;
-    audience?: string | string[] | undefined;
-    expiresAt?: number | Date | undefined;
-    expiresIn?: number | undefined;
-    notBefore?: number | Date | undefined;
-    issuedAt?: number | boolean | Date | undefined;
-    jwtId?: string | undefined;
-} | undefined): Promise<string>;
+}, keyStore: import('./key-store').KeyStore, { now, issuer, subject, audience, expiresAt, expiresIn, notBefore, issuedAt, jwtId }?: SignOptions | undefined): Promise<string>;
+/**
+ * @typedef {object} VerifyOptions
+ * @property {Date} [now] Date to use for all clock calculations
+ * @property {string|string[]} [issuer]
+ * @property {string} [subject]
+ * @property {string|RegExp|(string|RegExp)[]} [audience]
+ * @property {string} [jwtId]
+ * @property {number} [clockTolerance]
+ * @property {number} [maxAge]
+ */
 /**
  * @param {string} token
  * @param {import('./key-store').KeyStore} keyStore
- * @param {object} [options]
- * @param {Date} [options.now] Date to use for all clock calculations
- * @param {string|string[]} [options.issuer]
- * @param {string} [options.subject]
- * @param {string|RegExp|(string|RegExp)[]} [options.audience]
- * @param {string} [options.jwtId]
- * @param {number} [options.clockTolerance]
- * @param {number} [options.maxAge]
+ * @param {VerifyOptions} [options]
  */
-export function verify(token: string, keyStore: import('./key-store').KeyStore, { now, issuer, subject, audience, jwtId, clockTolerance, maxAge }?: {
+export function verify(token: string, keyStore: import('./key-store').KeyStore, { now, issuer, subject, audience, jwtId, clockTolerance, maxAge }?: VerifyOptions | undefined): Promise<{
+    [key: string]: unknown;
+}>;
+export type SignOptions = {
+    /**
+     * Date to use for all clock calculations
+     */
+    now?: Date | undefined;
+    /**
+     * identifies the principal that issued the JWT
+     */
+    issuer?: string | undefined;
+    /**
+     * identifies the principal that is the subject of the JWT
+     */
+    subject?: string | undefined;
+    /**
+     * identifies the recipients that the JWT is intended for
+     */
+    audience?: string | string[] | undefined;
+    expiresAt?: number | Date | undefined;
+    expiresIn?: number | undefined;
+    /**
+     * identifies the time before which the JWT MUST NOT be accepted for processing
+     */
+    notBefore?: number | Date | undefined;
+    /**
+     * identifies the time at which the JWT was issued
+     */
+    issuedAt?: number | boolean | Date | undefined;
+    /**
+     * provides a unique identifier for the JWT
+     */
+    jwtId?: string | undefined;
+};
+export type VerifyOptions = {
+    /**
+     * Date to use for all clock calculations
+     */
     now?: Date | undefined;
     issuer?: string | string[] | undefined;
     subject?: string | undefined;
@@ -45,6 +79,4 @@ export function verify(token: string, keyStore: import('./key-store').KeyStore, 
     jwtId?: string | undefined;
     clockTolerance?: number | undefined;
     maxAge?: number | undefined;
-} | undefined): Promise<{
-    [key: string]: unknown;
-}>;
+};
